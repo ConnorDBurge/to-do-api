ee5b5aeaec516e2e527290c0f65d27ca375e74f6
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-12 17:04:16 -0800

    finished UserProfile

--- to_do_api/views.py
+++ to_do_api/views.py
@@ -17,14 +17,14 @@ from .permissions import UpdateOwnProfile
 class UserProfileViewSet(viewsets.ModelViewSet):
     # api/profile/ (GET, POST)
     # api/profile/:id (GET, POST, PUT, PATCH, DELETE)
-    # api/profile/?search=<TERM>
     """Handle creating and updating profiles"""
-    serializer_class = UserProfileSerializer
-    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer # How are we serializing the profile?
+    queryset = UserProfile.objects.all() # Whats the dataset?
     authentication_classes = (TokenAuthentication,) # tuple
     permission_classes = (UpdateOwnProfile,) # tuple
+    # api/profile/?search=<TERM>
     filter_backends = (filters.SearchFilter,) # tuple
-    search_fields = ('name', 'email',) # tuple
+    search_fields = ('name', 'first_name', 'last_name',) # tuple
     
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
